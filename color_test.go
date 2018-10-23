fd943d8b899dcb79120b735d06b8a5f4e915f894
Wang Zuo <wangzuo@tiancai.pro>
2018-10-23 18:04:52 +0800

    add color.go

---
+++ color_test.go
@@ -0,0 +1,18 @@
+package color
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestHex2RGB(t *testing.T) {}
+
+func TestHSL2Hex(t *testing.T) {
+	hex := HSL2Hex(238, 116, 45)
+	fmt.Println("HSL2Hex", hex)
+}
+
+func TestHSV2RGB(t *testing.T) {
+	r, g, b := HSV2RGB(4, 59, 93)
+	fmt.Println("HSV2RGB", r, g, b)
+}
